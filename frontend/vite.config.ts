f06c258c2b879ce8defaf4e0b6f551385dd68644
Isaac Blanco <nuclearhands2001@gmail.com>
2026-09-12 15:31:17 -0500

    fix: missings deps for the test in the UI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'node',
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: [],
   },
 })
